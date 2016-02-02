ac26f548c30430db9bc3bde893cb9b4dc476c98e
Tomasz Dobrowolski <chrumck@gmail.com>
2016-02-01 23:12:48 -0800

    20160201-2312

--- src/main/java/Percolation.java
+++ src/main/java/Percolation.java
@@ -1,8 +1,26 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class Percolation {
+    private WeightedQuickUnionUF sites;
+    private boolean[] openSites;
+    private int N;
 
     // create N-by-N grid, with all sites blocked
     public Percolation(int N) {
+        if (N < 1) {
+            throw new IllegalArgumentException("N has to be larger than 0");
+        }
+        this.N = N;
+        sites = new WeightedQuickUnionUF(N ^ 2 + 2);
+        openSites = new boolean[N ^ 2 + 2];
+    }
 
+    //translate x,y to position in sites
+    private int xyTo1D(int x, int y) {
+        if (x < 1 || x > N || y < 1 || y > N) {
+            throw new IllegalArgumentException("x or y out of range");
+        }
+        return x + N * (y - 1);
     }
 
     // open site (row i, column j) if it is not open already
--- src/test/java/TestsPercolation.java
+++ src/test/java/TestsPercolation.java
@@ -1,29 +1,33 @@
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class TestsPercolation {
-
+    private Percolation perc;
+    public static final int N = 3;
 
     @Before
     public void setUp() throws Exception {
-
-
+        perc = new Percolation(N);
     }
 
     @After
     public void tearDown() throws Exception {
 
-
     }
 
     @Test
-    public void testPercolates()  {
-        System.out.println("foo");
-        assertEquals("","");
+    public void Percolation_throwsIfNlessThan1(){
+        try {
+            perc = new Percolation(0);
+        } catch (IllegalArgumentException e) {
+            assertTrue(e.getMessage().contains("N has to be larger than 0"));
+            return;
+        }
+        assertTrue(false);
     }
-
 
 }
