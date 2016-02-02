84c6edc216ae95accb7d626bf44f0ea92c02cfd3
Tomasz Dobrowolski <chrumck@gmail.com>
2016-02-01 19:39:21 -0800

    InitialComit

---
+++ src/test/java/TestsPercolation.java
@@ -0,0 +1,29 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestsPercolation {
+
+
+    @Before
+    public void setUp() throws Exception {
+
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+
+    }
+
+    @Test
+    public void testPercolates()  {
+        System.out.println("foo");
+        assertEquals("","");
+    }
+
+
+}
