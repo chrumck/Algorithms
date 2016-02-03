310fff6743afcecbf7481739f0d8cdeba0140c59
Tomasz Dobrowolski <chrumck@gmail.com>
2016-02-02 23:11:12 -0800

    20160202-2310

--- src/main/java/Percolation.java
+++ src/main/java/Percolation.java
@@ -1,9 +1,10 @@
-import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+import edu.princeton.cs.algs4.*;
 
 public class Percolation {
     private WeightedQuickUnionUF sites;
-    private boolean[] openSites;
     private int N;
+    private boolean[] openSites;
+    private boolean percolates = false;
 
     // create N-by-N grid, with all sites blocked
     public Percolation(int N) {
@@ -11,36 +12,55 @@ public class Percolation {
             throw new IllegalArgumentException("N has to be larger than 0");
         }
         this.N = N;
-        sites = new WeightedQuickUnionUF(N ^ 2 + 2);
-        openSites = new boolean[N ^ 2 + 2];
+        openSites = new boolean[N * N + 1];
+        sites = new WeightedQuickUnionUF(N * N + 1);
+        for (int i = 1; i < N + 1; i++) {
+            sites.union(i, 0);
+        }
     }
 
     //translate x,y to position in sites
-    private int xyTo1D(int x, int y) {
-        if (x < 1 || x > N || y < 1 || y > N) {
-            throw new IllegalArgumentException("x or y out of range");
+    private int xyTo1D(int i, int j) {
+        if (i < 1 || i > N || j < 1 || j > N) {
+            throw new IllegalArgumentException("i or j out of range");
         }
-        return x + N * (y - 1);
+        return i + N * (j - 1);
     }
 
     // open site (row i, column j) if it is not open already
     public void open(int i, int j) {
-
+        int sIndex = xyTo1D(i, j);
+        openSites[sIndex] = true;
+        if (i > 1 && openSites[sIndex - 1]) {
+            sites.union(sIndex, sIndex - 1);
+        }
+        if (i < N && openSites[sIndex + 1]) {
+            sites.union(sIndex, sIndex + 1);
+        }
+        if (j > 1 && openSites[sIndex - N]) {
+            sites.union(sIndex, sIndex - N);
+        }
+        if (j < N && openSites[sIndex + N]) {
+            sites.union(sIndex, sIndex + N);
+        }
+        if (!percolates && j == N && sites.connected(sIndex, 0)) {
+            percolates = true;
+        }
     }
 
     // is site (row i, column j) open?
     public boolean isOpen(int i, int j) {
-        return true;
+        return openSites[xyTo1D(i, j)];
     }
 
     // is site (row i, column j) full?
     public boolean isFull(int i, int j) {
-        return true;
+        return sites.connected(xyTo1D(i, j), 0);
     }
 
     // does the system percolate?
     public boolean percolates() {
-        return true;
+        return percolates;
     }
 
 }
